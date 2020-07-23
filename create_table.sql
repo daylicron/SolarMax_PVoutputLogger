6de80cbaa77d78f05e188d5b4fa1505a661a95a0
Dennis Well <d.well@babiel.com>
2020-07-23 19:25:09 +0200

    feat(create table): provide create command for table

---
+++ create_table.sql
@@ -0,0 +1,1 @@
+create table <table_name>( id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY, datum DATETIME, nrwr TINYINT, pac FLOAT, tagesertrag FLOAT, monatsertrag INT, jahresertrag INT, gesamtertrag INT, temperatur FLOAT, udc1 FLOAT, idc1 FLOAT, status VARCHAR(255), fehler VARCHAR(255) );
